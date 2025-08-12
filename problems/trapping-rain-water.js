eaa96bc53aeb5307bdecce5d6e30a8e4b93b1558
Tanvir Azad <tanvirazad49@gmail.com>
2025-08-12 16:45:56 +0200

    Refactor trapping rain water

--- problems/trapping-rain-water.js
+++ problems/trapping-rain-water.js
@@ -19,19 +19,47 @@
 // Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
 // Output: 6
 
-//TODO: try optimizing
-function f(heights){
-    let totalWater = 0
-    for(let i = 0; i < heights.length; i++){
-        if(i === 0 || i === heights.length - 1) continue
+// time: O(n^2)
+// function f(heights){
+//     let totalWater = 0
+//     for(let i = 0; i < heights.length; i++){
+//         if(i === 0 || i === heights.length - 1) continue
         
-        const maxLeftBarHeight = Math.max(...heights.slice(0,i))
-        const maxRightBarHeight = Math.max(...heights.slice(i+1, heights.length))
-        const water = Math.min(maxLeftBarHeight, maxRightBarHeight) - heights[i]
-        
-        if(water > 0) totalWater += water
-    }
-    return totalWater
-}
+//         const maxLeftBarHeight = Math.max(...heights.slice(0,i))
+//         const maxRightBarHeight = Math.max(...heights.slice(i+1, heights.length))
+//         const water = Math.min(maxLeftBarHeight, maxRightBarHeight) - heights[i]
+//         console.log({water})
+//         if(water > 0) totalWater += water
+//     }
+//     return totalWater
+// }
 
-console.log(f([0,1,0,2,1,0,1,3,2,1,2,1]))
+// time: O(n) space: O(n)
+// function f(heights){
+//     let totalWater = 0
+//     let leftMaxHeights = Array(heights.length).fill(0)
+//     let rightMaxHeights = Array(heights.length).fill(0)
+
+//     for (let i = 1; i < heights.length; i++) {
+//         leftMaxHeights[i] = Math.max(leftMaxHeights[i-1], heights[i-1])
+//     }
+
+//     for (let i = heights.length - 2; i > -1; i--) {
+//         rightMaxHeights[i] = Math.max(rightMaxHeights[i+1], heights[i+1])
+//     }
+
+//     for (let i = 0; i < heights.length; i++) {
+//         const h = heights[i];
+//         const water = Math.min(leftMaxHeights[i], rightMaxHeights[i]) - h
+//         if(water > 0) totalWater+= water
+        
+//     }
+
+//     return totalWater
+// }
+
+// TODO: try with 2 pointer
+
+console.log(f([0,1,0,2,0])) // 1
+
+console.log(f([0,1,0,2,1,0,1,3,2,1,2,1])) // 6
