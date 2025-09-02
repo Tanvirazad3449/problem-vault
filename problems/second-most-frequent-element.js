8944eebd59d1581bd917b511a5269c0b1e6d95d4
Tanvir Azad <tanvirazad49@gmail.com>
2025-09-02 13:26:57 +0200

    Refactor second most frequent element function and add line-people problem description

--- problems/second-most-frequent-element.js
+++ problems/second-most-frequent-element.js
@@ -4,25 +4,27 @@ import test from '../test.mjs'
 // You are given an array of elements (which may contain integers, strings, or other hashable values). 
 // Your task is to find the element that occurs with the second-highest frequency in the array.
 function f(a){
-    let freq = {}, maxFreq = 0, secondMostElem = null, secondmaxFreq = 0
-    for (let i = 0; i < a.length; i++){
-        freq[a[i]] = (freq[a[i]] || 0) + 1
+    // find maxfreq count and the item that appeared max times, maybe using a map to store
+
+    let map = new Map(), maxFreq = 0, secondMostFreq = 0, secondMostFreqItem = null
+
+    for(const item of a){
+        map.set(item, (map.get(item) || 0) + 1)
     }
 
-    for(const key in freq){
-        if(maxFreq < freq[key]){
-            maxFreq = freq[key]
+    for(const count of map.values()){
+        if(count > maxFreq) maxFreq = count
+    }
+
+    for(const [item, count] of map.entries()){
+        if(count > secondMostFreq && count < maxFreq){
+            secondMostFreq = count
+            secondMostFreqItem = item
         }
     }
 
-    for(const key in freq){
-        if(freq[key] < maxFreq && secondmaxFreq < freq[key]){
-            secondMostElem = key
-            secondmaxFreq = freq[key]
-        }
-    }
+    return secondMostFreqItem
 
-    return Number(secondMostElem) || secondMostElem
 }
 test([
     {input: [[1, 2, 2, 3, 3, 3, 4, 4]], expected: [2, 1]},       // or 4 (tie acceptable)
